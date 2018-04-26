3d24af8ef48093397eb0cb208df4a5f167cb8a07
Abhishek Pawar <youremail@domain.com>
2018-04-26 21:20:24 +0530

    Shared Module created

---
+++ lzw/lzw_shared.hs
@@ -0,0 +1,28 @@
+
+Module Shared (Id,Abbreviation,CompDict,DecompDict,initCompDict,initDecompDict) where
+  
+
+import System.Environment
+
+import GHC.Word
+
+import qualified Data.Map as M
+
+-- Index in the table
+type Id = Word16
+
+type Abbreviation = (Id,Word8)
+
+-- Type of Dictionary used for compression
+type CompDict = M.Map Abbreviation Id
+
+-- Function to initialize the compression dictionary
+initCompDict :: CompDict
+initCompDict = M.fromList [((0,n), fromIntegral(n + 1)) | n <- [0..255]]
+
+-- Type of Dictionary used for decompression
+type DecompDict = M.Map Id Abbreviation
+
+-- Function to initialize the compression dictionary
+initDecompDict :: DecompDict
+initDecompDict = M.fromList $ map swap $ M.toList initCompDict
