5e4366e8e1406764e5beaabafea684790edfff28
Ninad Akolekar <ninad.akolekar@gmail.com>
2018-04-28 00:13:26 +0530

    Bit-Byte Conversion Procedures Huffman

--- huff/shared_huff.hs
+++ huff/shared_huff.hs
@@ -3,10 +3,11 @@
    modules.
  -}
 module Huffman_shared (
-
         -- ** Common Operations on Huffman Compression and Decompression
         readInputFile,
-        printCompFile
+        printCompFile,
+        bytesToBits,
+        bitsToBytes
 )
 
 where
@@ -38,6 +39,40 @@ where
     printCompFile :: String -> [Byte] -> IO()
 
     printCompFile inputFile = BS.writeFile inputFile . BS.pack
+
+    {- |
+        Function that accepts a list of booleans (bits) and outputs a list of bytes
+    -}
+    bitsToBytes :: [Bool] -> [Byte]
+
+    bitsToBytes boolList = reverse $ bitsToBytesRev [] boolList 0 0 where
+
+        -- Base Case
+        bitsToBytesRev x [] xi 8 = xi:x
+
+        -- Recursive definition
+        bitsToBytesRev x [] xi n = bitsToBytesRev x [] (xi*2) (n+1)
+        bitsToBytesRev x list xi 8 = bitsToBytesRev (x1:x) list 0 0
+        bitsToBytesRev x (False:list) xi n = bitsToBytesRev x list (xi*2) (n+1)
+        bitsToBytesRev x (True:list) xi n = bitsToBytesRev x list (xi*2+1) (n+1)
+    
+    
+    {- |
+        Function that accepts a byte (@Byte) and outputs the corresponding list of bits.
+    -}
+    byteToBits :: Byte -> [Bool]
+
+    bytesToBits = reverse . bytesToBitsRev 8 where
+
+        -- Base Case
+        bytesToBitsRev 0 _ = []
+
+        -- Recursive Definition
+        bytesToBitsRev n x 
+            | even x = False : bytesToBitsRev (n-1) (div x 2)
+            | otherwise = True : bytesToBitsRev (n-1) (div x 2)
+
+    
 
 
     
