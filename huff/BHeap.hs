e2ee7ab5b39f03fd85d28c692efa46f6243d8401
Ninad Akolekar <ninad.akolekar@gmail.com>
2018-04-27 21:10:54 +0530

    Binomial Heap Procedures

--- huff/BHeap.hs
+++ huff/BHeap.hs
@@ -9,9 +9,9 @@ module BinomialHeap (
 )
 where
 
-data BinomialTree a = Node Int a [BinomialTree a] deriving (Display, Read)
+data BinomialTree a = Node Int a [BinomialTree a] deriving (Show, Read)
 
-data BinomialHeap a = BH [BinomialTree a] deriving (Display, Read)
+data BinomialHeap a = BH [BinomialTree a] deriving (Show, Read)
 
 -- | Get the height of the binomial tree
 height :: BinomialTree a -> Int
@@ -65,3 +65,34 @@ mergeTrees ts1@(t1:tr1) ts2@(t2:tr2)
     height t2 < height t1 = t2 : mergeTrees ts1 tr2
     otherwise = insertTree (linkTrees t1 t2) (mergeTrees tr1 tr2)
 
+
+
+{- | Auxillary function that accepts a @BinomialTree@ and removes the minimum.
+    | Returns minimum and the remainder tree.
+-}
+deleteMinimumTree :: (Ord a) => [BinomialTree a] -> (BinomialTree a, [BinomialTree a])
+
+-- Base Cases
+
+deleteMinimumTree [] = error "Error (deleteMinimumTree) : Empty Heap!"
+deleteMinimumTree [t] = (t,[])
+
+-- Recursive Definition
+deleteMinimumTree (t:ts) = if root t < root tRem then (t,ts) else (tRem, t:tsRem) where
+    (tRem,tsRem) = deleteMinimumTree ts
+
+
+{- | Function that accepts a @BinomialHeap@ and returns the minimum element.
+-}
+findMinimum :: (Ord a) => BinomialHeap a -> a
+
+findMinimum (BH ts) = root t
+    where (t,_) = deleteMinimumTree ts
+
+
+{- | Function that accepts a @BinomialHeap@ and eliminates the minimum
+-}
+deleteMinimum :: (Ord a) => BinomialHeap a -> BinomialHeap a
+
+deleteMinimum (BH ts) = BH (mergeTrees (reverse ts1) ts2) where
+    (Node _ x ts1, ts2) = deleteMinimumTree ts
