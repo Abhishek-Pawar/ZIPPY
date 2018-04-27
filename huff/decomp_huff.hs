8394b0ee78864c5f66cdc63b2ab24e4dd2854cb5
Abhishek Pawar <youremail@domain.com>
2018-04-28 02:28:45 +0530

    Completed Huffman Decompression procedures

--- huff/decomp_huff.hs
+++ huff/decomp_huff.hs
@@ -18,3 +18,51 @@ makeHuffTree list            = Branch 0 (makeHuffTree left) (makeHuffTree right)
     where
         left =  [(e,xs)| (e,(False:xs)) <- list]
         right = [(e,xs)| (e,(True:xs))  <- list]
+
+{- |
+  Lee una codificacion de un Arbol de Huffman y reconstruye el
+  Arbol del que fue generado, separando el resto de los datos binarios.
+  (El Arbol de Huffman generado tendra frecuencias acumuladas triviales,
+  iguales a cero).
+ -}
+decodeHuff :: [Bool] -> (Huffman Byte, [Bool])
+decodeHuff code = (huff, trim rem)
+    where
+        (attr, rem) = parseAssocsInit [] code
+        huff           = makeHuffTree attr
+        trim           = reverse . tail . dropWhile not . reverse
+        parseAssocsInit accum (True:False:xs) = (accum, xs)
+        parseAssocsInit accum (True:True:xs)  = parseAssocs accum [True] xs
+        parseAssocs accum code (False:xs)      = parseAssocsInit ((head $ bitsToBytes e, code):accum) r
+            where (e, r) = splitAt 8 xs
+        parseAssocs accum code (True:x:xs)     = parseAssocs accum (x:code) xs
+
+
+{- |
+ - Given a Huffman tree and a list that represents a chain of bits,
+   returns a list of symbols that corresponds to the decoding of said
+   chain with the tree's information
+ -}
+huffDecomp :: Huffman a -> [Bool] -> [a]
+huffDecomp a [] = []
+huffDecomp a c  = fetchSym a c []
+    where
+        fetchSym (Leaf _ e)     []         acc = acc
+        fetchSym (Leaf _ e)     (_:rest)   acc = acc ++ [e] ++ (huffDecomp a rest)
+        fetchSym (Branch _ _ d) (True:xs)  acc = fetchSym d xs acc
+        fetchSym (Branch _ i _) (False:xs) acc = fetchSym i xs acc
+
+
+
+{- |
+  Transforms a byte list into another one, compressing or decompressing it
+  as specified (True is compress, False is decompress).
+  When compressing, the last True is added in order to have an end of file character
+  (All of the False found before the last True will be ignored by the decoder)
+ -}
+
+huffDecompress :: [Byte] -> [Byte]
+
+huffDecompress code = huffDecomp huff cods
+    where
+        (huff, cods) = decodeHuff . concat $ map byteToBits code
